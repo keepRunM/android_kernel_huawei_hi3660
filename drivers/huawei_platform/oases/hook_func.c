93190ac0d91d32ad017d48690576d592d63aae1e
keepRunM <411384073@qq.com>
2023-03-25 14:24:02 +0800

    Update hook_func.c

--- drivers/huawei_platform/oases/hook_func.c
+++ drivers/huawei_platform/oases/hook_func.c
@@ -158,12 +158,12 @@ static int func_pre_setup_trampoline(struct oases_patch_entry *patch,
     memcpy(trampoline, &oases_handler_func_pre_start,
         &oases_handler_func_pre_end - &oases_handler_func_pre_start);
 	/* NOP */
-    *((u32 *)(trampoline + ##FUNC_PRE_PLACE_HOLDER_OFFSET)) = 0xAA0003E0;
-    *((u64 *)(trampoline + ##FUNC_PRE_FILTER_ADDR_OFFSET)) = (u64)(insn->handler);
-    *((u64 *)(trampoline + ##FUNC_PRE_ORIGIN_ADDR_OFFSET)) = (u64)(insn->address + 4);
-    *((u64 *)(trampoline + ##FUNC_PRE_PATCH_INFO_CTX_OFFSET)) = (u64)(kp_owner(patch));
-    *((u64 *)(trampoline + ##FUNC_PRE_ATTACK_LOGGER_OFFSET)) = (u64)(oases_attack_logger);
-    oases_relocate_insn(insn, ##FUNC_PRE_PLACE_HOLDER_OFFSET);
+    *((u32 *)(trampoline + 116)) = 0xAA0003E0;
+    *((u64 *)(trampoline + 140)) = (u64)(insn->handler);
+    *((u64 *)(trampoline + 148)) = (u64)(insn->address + 4);
+    *((u64 *)(trampoline + 156)) = (u64)(kp_owner(patch));
+    *((u64 *)(trampoline + 164)) = (u64)(oases_attack_logger);
+    oases_relocate_insn(insn, 116);
     return 0;
 }
 
@@ -196,12 +196,12 @@ static int func_post_setup_trampoline(struct oases_patch_entry *patch, struct oa
     memcpy(trampoline, &oases_handler_func_post_start,
         &oases_handler_func_post_end - &oases_handler_func_post_start);
 	/* NOP */
-    *((u32 *)(trampoline + ##FUNC_POST_PLACE_HOLDER_OFFSET)) = 0xAA0003E0;
-    *((u64 *)(trampoline + ##FUNC_POST_FILTER_ADDR_OFFSET)) = (u64)(insn->handler);
-    *((u64 *)(trampoline + ##FUNC_POST_ORIGIN_ADDR_OFFSET)) = (u64)(insn->address + 4);
-    *((u64 *)(trampoline + ##FUNC_POST_PATCH_INFO_CTX_OFFSET)) = (u64)(kp_owner(patch));
-    *((u64 *)(trampoline + ##FUNC_POST_ATTACK_LOGGER_OFFSET)) = (u64)(oases_attack_logger);
-    oases_relocate_insn(insn, ##FUNC_POST_PLACE_HOLDER_OFFSET);
+    *((u32 *)(trampoline + 116)) = 0xAA0003E0;
+    *((u64 *)(trampoline + 140)) = (u64)(insn->handler);
+    *((u64 *)(trampoline + 148)) = (u64)(insn->address + 4);
+    *((u64 *)(trampoline + 156)) = (u64)(kp_owner(patch));
+    *((u64 *)(trampoline + 164)) = (u64)(oases_attack_logger);
+    oases_relocate_insn(insn, 116);
 	return 0;
 }
 
